7ca31212050922aa447b9ab6ac2942a5c3e06f1e
rezkynovitaa <85878732+rezkynovitaa@users.noreply.github.com>
2021-08-28 11:01:31 +0800

    Syntax ARIMA Non Musiman

--- Arima Non Musimann.R
+++ Arima Non Musimann.R
@@ -8,9 +8,9 @@ library(nortest)
 
 #mempersiapkan data
 #mengganti label zt dengan penjualan
-penjualan<-penjualanproduk$zt
+penjualan<-data.penjualan$zt
 penjualan
-hari<-penjualanproduk$t
+hari<-data.penjualan$t
 
 #membuat plot time series
 ts.plot(penjualan)
@@ -23,16 +23,29 @@ par(mfrow=c(1,2))
 acf(penjualan,lag.max = 46)
 pacf(penjualan,lag.max = 46)
 
+#differencing
+differencing<-diff(penjualan,differences = 1)
+differencing
+ts.plot(differencing)
+adf.test(differencing)
+
+differencing2<-diff(penjualan,differences = 2 )
+differencing2
+ts.plot(differencing2)
+adf.test(differencing2)
+
+par(mfrow=c(1,2))
+acf(differencing2,lag.max = 46)
+pacf(differencing2,lag.max = 46)
+
 #menduga ARIMA
 fit1<-Arima(penjualan,order = c(0,2,2),method = "ML")
 fit1
 #diagnostig checking
 #1 Uji kesignifikanan uji t
 coeftest(fit1)
-
 #2 white noise
 Box.test(fit1$residuals, type="Ljung")
-
 #3. Penujian residual apakah berdistribusi normal
 shapiro.test(fit1$residuals)
 #Shapiro-Francia normality test
@@ -42,6 +55,6 @@ ad.test(fit1$residuals)
 #Lilliefors (Kolmogorov-Smirnov) normality test
 lillie.test(fit1$residuals)
 
-#forecast pada 3 tahap kedepan
+#forecast
 forecastt<-forecast(penjualan,model=fit1,h=3)
 forecastt
